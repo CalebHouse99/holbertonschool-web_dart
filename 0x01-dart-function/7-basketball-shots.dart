e7bf45cd32a8e1ca66487b311a279006ec1dc688
CalebHouse99 <78427735+CalebHouse99@users.noreply.github.com>
2023-10-16 19:50:40 +0000

    wow dart is picky about it potentially being null

---
+++ 0x01-dart-function/7-basketball-shots.dart
@@ -0,0 +1,14 @@
+int whoWins(Map<String, int> teamA, Map<String, int> teamB) {
+    int totalA = (teamA['Free throws'] ?? 0) + (teamA['2 pointer'] ?? 0) * 2 + (teamA['3 pointer'] ?? 0) * 3;
+    int totalB = (teamB['Free throws'] ?? 0) + (teamB['2 pointer'] ?? 0) * 2 + (teamB['3 pointer'] ?? 0) * 3;
+
+    if (totalA > totalB) {
+        return 1;
+    }
+    else if (totalB > totalA) {
+        return 2;
+    }
+    else {
+        return 0;
+    }
+}
