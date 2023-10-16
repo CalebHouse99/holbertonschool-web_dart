8f89a69cb5b39c96fd89ffec30692eb2a516d456
CalebHouse99 <78427735+CalebHouse99@users.noreply.github.com>
2023-10-16 16:52:28 +0000

    task 6

---
+++ 0x00-hello-dart/6-positive_or_negative.dart
@@ -0,0 +1,11 @@
+void main(List<String> args) {
+    if (int.parse(args[0]) > 0){
+        print('${args[0]} is positive');
+    }
+    else if (int.parse(args[0]) < 0){
+        print('${args[0]} is negative');
+    }
+    else {
+        print('${args[0]} is zero');
+    }
+}
