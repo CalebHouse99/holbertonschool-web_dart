e04167d86bacb8e7ba1a17aabb9a6fba3f582b2f
CalebHouse99 <78427735+CalebHouse99@users.noreply.github.com>
2023-10-16 20:04:27 +0000

    task 7....pls work

--- 0x01-dart-function/7-basketball-shots.dart
+++ 0x01-dart-function/7-basketball-shots.dart
@@ -1,16 +1,14 @@
 int whoWins(Map<String, int> teamA, Map<String, int> teamB) {
-    int totalA = (teamA['Free throws'] ?? 0) + (teamA['2 pointer'] ?? 0) * 2 + (teamA['3 pointer'] ?? 0) * 3;
-    int totalB = (teamB['Free throws'] ?? 0) + (teamB['2 pointer'] ?? 0) * 2 + (teamB['3 pointer'] ?? 0) * 3;
+  int scoreA = (teamA['Free throws'] ?? 0) 
+             + (teamA['2 pointer'] ?? 0) * 2 
+             + (teamA['3 pointer'] ?? 0) * 3;
 
-    // Debugging prints
-    // print('Team A score: $totalA');
-    // print('Team B score: $totalB');
-    
-    if (totalA > totalB) {
-        return 1;
-    } else if (totalB > totalA) {
-        return 2;
-    } else {
-        return 0;
-    }
+  int scoreB = (teamB['Free throws'] ?? 0) 
+             + (teamB['2 pointer'] ?? 0) * 2 
+             + (teamB['3 pointer'] ?? 0) * 3;
+
+  if (scoreA > scoreB) return 1;
+  if (scoreA < scoreB) return 2;
+  return 0;
 }
+
