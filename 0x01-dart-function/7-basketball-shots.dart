25ba2e022eebd3ea85b45816160e2d46b70e1722
CalebHouse99 <78427735+CalebHouse99@users.noreply.github.com>
2023-10-16 20:01:56 +0000

    debugging. it should work now??

--- 0x01-dart-function/7-basketball-shots.dart
+++ 0x01-dart-function/7-basketball-shots.dart
@@ -2,13 +2,15 @@ int whoWins(Map<String, int> teamA, Map<String, int> teamB) {
     int totalA = (teamA['Free throws'] ?? 0) + (teamA['2 pointer'] ?? 0) * 2 + (teamA['3 pointer'] ?? 0) * 3;
     int totalB = (teamB['Free throws'] ?? 0) + (teamB['2 pointer'] ?? 0) * 2 + (teamB['3 pointer'] ?? 0) * 3;
 
+    // Debugging prints
+    // print('Team A score: $totalA');
+    // print('Team B score: $totalB');
+    
     if (totalA > totalB) {
         return 1;
-    }
-    else if (totalB > totalA) {
+    } else if (totalB > totalA) {
         return 2;
-    }
-    else {
+    } else {
         return 0;
     }
 }
--- 0x01-dart-function/7-main.dart
+++ 0x01-dart-function/7-main.dart
@@ -1,8 +1,15 @@
 import '7-basketball-shots.dart';
 
 main() {
-  var teamA = {'Free throws' : 23, '2 pointer' : 32, '3 pointer': 16};
-  var teamB = {'Free throws' : 31, '2 pointer' : 27, '3 pointer': 13};
-
-  print(whoWins(teamA, teamB));
+  var teamA1 = {'Free throws': 23, '2 pointer': 32, '3 pointer': 16};
+  var teamB1 = {'Free throws': 31, '2 pointer': 27, '3 pointer': 13};
+  print(whoWins(teamA1, teamB1)); // Should print 1
+  
+  var teamA2 = {'Free throws': 23, '2 pointer': 32, '3 pointer': 16};
+  var teamB2 = {'Free throws': 31, '2 pointer': 37, '3 pointer': 13};
+  print(whoWins(teamA2, teamB2)); // Should print 2
+  
+  var teamA3 = {'Free throws': 23, '2 pointer': 32, '3 pointer': 16};
+  var teamB3 = {'Free throws': 21, '2 pointer': 33, '3 pointer': 16};
+  print(whoWins(teamA3, teamB3)); // Should print 0
 }
