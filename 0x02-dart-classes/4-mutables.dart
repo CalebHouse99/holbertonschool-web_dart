bf3153240b529be34a5d3c5baf67ce9d5eea6f82
unknown <calebhouse99@gmail.com>
2023-10-18 20:56:22 -0500

    4-6

---
+++ 0x02-dart-classes/4-mutables.dart
@@ -0,0 +1,40 @@
+class Password {
+  String _password;
+
+  Password({String password = ""}) : _password = password;
+
+  bool isValid() {
+    if (_password.length < 8 || _password.length > 16) {
+      return false;
+    }
+
+    bool hasUppercase = false;
+    bool hasLowercase = false;
+    bool hasNumber = false;
+
+    for (int i = 0; i < _password.length; i++) {
+      if (_password[i].toUpperCase() != _password[i]) {
+        hasLowercase = true;
+      }
+      if (_password[i].toLowerCase() != _password[i]) {
+        hasUppercase = true;
+      }
+      if (int.tryParse(_password[i]) != null) {
+        hasNumber = true;
+      }
+    }
+
+    return hasUppercase && hasLowercase && hasNumber;
+  }
+
+  String get password => _password;
+
+  set password(String newPassword) {
+    _password = newPassword;
+  }
+
+  @override
+  String toString() {
+    return "Password $_password";
+  }
+}
