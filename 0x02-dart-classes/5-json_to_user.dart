01f232b9180e3ca122ad25470e4a1f06829b9f99
unknown <calebhouse99@gmail.com>
2023-10-18 21:12:06 -0500

    spacing

--- 0x02-dart-classes/5-json_to_user.dart
+++ 0x02-dart-classes/5-json_to_user.dart
@@ -25,7 +25,7 @@ class User {
   }
 
   @override
-  String toString() {
-    return 'User(id : $id ,name: $name,  age: $age, height: $height)';
-  }
+    String toString() {
+        return 'User(id: $id, name: $name, age: $age, height: $height)';
+    }
 }
